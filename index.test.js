53cb11f62ad1d904848dd0daa30fa892942ccb19
ASAT1234 <117856678+ASAT1234@users.noreply.github.com>
2022-11-10 00:33:37 +0300

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,9 +1,9 @@
 const { mooncakeSays } = require('./');
 
 test('mooncake knows what to say', () => {
-  expect(mooncakeSays('')).toBe('Hello we are: Saleh, asem, Nawaf, Aziz');
+  expect(mooncakeSays('')).toBe('Hello we are: Saleh, asem, Nawaf, Aziz , Abdullah');
 });
 
 test('mooncake is not sure what to say', () => {
-  expect(mooncakeSays()).toBe('Hello we are: Saleh, asem, Nawaf, Aziz');
+  expect(mooncakeSays()).toBe('Hello we are: Saleh, asem, Nawaf, Aziz , Abdullah');
 });
