b23df70d4620dad2480f06a23c434d07c1e5eb97
Asem-001 <117676536+Asem-001@users.noreply.github.com>
2022-11-09 23:54:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function mooncakeSays(message) {
   let mooncake = 'Hello iam: Saleh';
+  let mooncake2 = "Hello iam: asem"
 
   if ( !message ) {
     return `${mooncake}`;
   }
 
-  return `${mooncake} ${message}`;
+  return `${mooncake} ${mooncake2}`;
 }
 
 module.exports.mooncakeSays = mooncakeSays;
