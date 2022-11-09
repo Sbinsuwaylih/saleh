a8c460986d4bd548912b5c85ba25a322ede63482
Sbinsuwaylih <117676731+Sbinsuwaylih@users.noreply.github.com>
2022-11-09 23:37:11 +0300

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 const { mooncakeSays } = require('./');
 
 test('mooncake knows what to say', () => {
-  expect(mooncakeSays(' ')).toBe('Hello iam:');
+  expect(mooncakeSays('')).toBe('Hello iam:');
 });
 
 test('mooncake is not sure what to say', () => {
